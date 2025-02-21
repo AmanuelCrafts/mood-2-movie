a52b7394e8ab3dccb5b85a88e23b4f13134a4609
AmanuelCrafts <amanuelabebawfr@gmail.com>
2025-02-22 00:17:53 +0300

    Implemented Analytics

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,12 @@ import Home from "./pages/Home";
 import MovieContainer from "./pages/MovieContainer";
 import Footer from "./components/Footer";
 import NotFound from "./pages/NotFound";
+import { Analytics } from "@vercel/analytics/react";
 
 function App() {
   return (
     <div className="absolute min-h-screen w-full top-0 z-[-2] bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))] bg-cover">
+      <Analytics />
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
